beb1ec081e3d87cecd3a1767525ea09e9c12a2ed
Stanley Okeke <Stanley.Okeke@gov.bc.ca>
2022-08-24 12:19:27 -0700

    set up openshift cicd pipeline using tekotn and helm

--- backend/webapi/Features/AccessRequests/DigitalEvidence.cs
+++ backend/webapi/Features/AccessRequests/DigitalEvidence.cs
@@ -109,6 +109,7 @@ public class DigitalEvidence
                 return DomainResult.Failed();
             }
 
+            //sample
             //this.context.AccessRequests.Add(new AccessRequest
             //{
             //    PartyId = command.PartyId,
