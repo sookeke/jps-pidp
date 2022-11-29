de32b376b04a003eead5d0c776bcc34d1e51cd9a
Stanley Okeke <Stanley.Okeke@gov.bc.ca>
2022-11-29 12:51:09 -0800

    consumer retry logic and architecure diagram for edt

---
+++ backend/service.edt/Data/Migrations/20221129204729_FailedEvent.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace edt.service.Data.Migrations
+{
+    public partial class FailedEvent : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FailedEventLogs",
+                schema: "edt",
+                columns: table => new
+                {
+                    EventId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Producer = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ConsumerGroupId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ConsumerId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Modified = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FailedEventLogs", x => x.EventId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FailedEventLogs",
+                schema: "edt");
+        }
+    }
+}
